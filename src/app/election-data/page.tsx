990e66e131bfd02dd83a5933adc65f2a473c659d
Iris Huang <lyaui@outloook.com>
2023-11-23 03:18:48 +0800

    feat(ElectionDataPage): add selected area title

--- src/app/election-data/page.tsx
+++ src/app/election-data/page.tsx
@@ -1,13 +1,38 @@
 import Navbar from '@/components/layout/Navbar';
 import Map from '@/components/Map/index';
+import { QUERY } from '@/routers/index';
 
-function ElectionDataPage() {
+interface ElectionDataPageProps {
+  searchParams: {
+    [key: string]: string | undefined;
+    year?: string;
+    city?: string;
+    dist?: string;
+  };
+}
+
+function ElectionDataPage({ searchParams }: ElectionDataPageProps) {
+  const { [QUERY.CITY]: city = '', [QUERY.DIST]: dist = '' } = searchParams;
+
+  const title = (() => {
+    if (dist) return dist;
+    if (city) return city;
+    return '全臺縣市總統得票';
+  })();
+
   return (
     <div>
       <Navbar />
       <main className='flex mt-[65px]'>
         <Map />
-        <article className='h-[calc(100vh-65px)] px-12 py-8 overflow-auto '></article>
+        <article className='h-[calc(100vh-65px)] px-12 py-8 overflow-auto '>
+          {/* info */}
+          <h3 className='heading-3'>{title}</h3>
+
+          {/* overview */}
+          {/* party result */}
+          {/* area result */}
+        </article>
       </main>
     </div>
   );
