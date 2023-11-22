ea440b7cbaafb022c078654fffe3ea628ef79f51
Iris Huang <lyaui@outloook.com>
2023-11-23 00:27:15 +0800

    feat(ElectionDataPage): page layout

--- src/app/election-data/page.tsx
+++ src/app/election-data/page.tsx
@@ -1,9 +1,14 @@
 import Navbar from '@/components/layout/Navbar';
+import Map from '@/components/Map/index';
 
 function ElectionDataPage() {
   return (
     <div>
       <Navbar />
+      <main className='flex mt-[65px]'>
+        <Map />
+        <article className='h-[calc(100vh-65px)] px-12 py-8 overflow-auto '></article>
+      </main>
     </div>
   );
 }
---
+++ src/components/Map/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Map() {
+  return (
+    <article className='w-[500px] h-[calc(100vh-65px)] bg-gray-400 overflow-auto shrink-0'></article>
+  );
+}
+
+export default Map;
