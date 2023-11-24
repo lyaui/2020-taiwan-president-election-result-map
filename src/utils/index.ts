9cf625af400236d70a214c6e24eff332320788a3
Iris Huang <lyaui@outloook.com>
2023-11-25 02:39:20 +0800

    feat(utils): add transCommaStringToNumber

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,7 @@
 export function numberWithCommas(num: number) {
   return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
+
+export function transCommaStringToNumber(str: string) {
+  return parseInt(str.split(',').join(''));
+}
