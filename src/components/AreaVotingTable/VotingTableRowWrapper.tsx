9e1973c005a1d66197249981456f4f003cfbf934
Iris Huang <lyaui@outloook.com>
2023-11-25 22:27:37 +0800

    feat(VotingTableRowWrapper): add handleNextLevelClick event

--- src/components/AreaVotingTable/VotingTableRowWrapper.tsx
+++ src/components/AreaVotingTable/VotingTableRowWrapper.tsx
@@ -1,17 +1,39 @@
 'use client';
 import { type ReactNode } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
+import querystring from 'query-string';
+
+import { type Level } from '@/types';
+import { levels } from '@/constants';
+import { ROUTER } from '@/routers';
 
 interface VotingTableRowWrapperProps {
   children: ReactNode;
   placeName: string;
+  placeLevel: Level;
 }
 
 function VotingTableRowWrapper({
   children,
   placeName,
+  placeLevel,
 }: VotingTableRowWrapperProps) {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
   const handleNextLevelClick = () => {
-    console.log(placeName);
+    const levelArr = Object.values(levels);
+    const levelIdx = levelArr.indexOf(placeLevel);
+    if (levelIdx === levelArr.length - 1) return;
+
+    const queryString = querystring.stringify(
+      {
+        ...querystring.parse(searchParams.toString()),
+        [placeLevel]: placeName,
+      },
+      { skipEmptyString: true },
+    );
+    router.push(`${ROUTER.ELECTION_DATA}?${queryString}`);
   };
   return (
     <tr
