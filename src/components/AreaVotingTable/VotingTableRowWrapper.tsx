6fb30940db058d2629e9b95b1d07342ed4c6a9d2
Iris Huang <lyaui@outloook.com>
2023-11-27 01:31:26 +0800

    temp

--- src/components/AreaVotingTable/VotingTableRowWrapper.tsx
+++ src/components/AreaVotingTable/VotingTableRowWrapper.tsx
@@ -2,6 +2,7 @@
 import { type ReactNode } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import querystring from 'query-string';
+import TEST from 'public/json/2020/連江縣.json';
 
 import { type Level } from '@/types';
 import { levels } from '@/constants';
@@ -21,19 +22,74 @@ function VotingTableRowWrapper({
   const router = useRouter();
   const searchParams = useSearchParams();
 
-  const handleNextLevelClick = () => {
-    const levelArr = Object.values(levels);
-    const levelIdx = levelArr.indexOf(placeLevel);
-    if (levelIdx === levelArr.length - 1) return;
+  const data = Array.isArray(JSON.parse(JSON.stringify(TEST)))
+    ? JSON.parse(JSON.stringify(TEST))?.slice(5)
+    : [];
 
-    const queryString = querystring.stringify(
-      {
-        ...querystring.parse(searchParams.toString()),
-        [placeLevel]: placeName,
-      },
-      { skipEmptyString: true },
-    );
-    router.push(`${ROUTER.ELECTION_DATA}?${queryString}`);
+  const handleNextLevelClick = () => {
+    const test = (city, data) => {
+      const title = `第15任總統副總統選舉候選人在${city}各村(里)得票數一覽表`;
+      let name = '';
+
+      return data.reduce(
+        (_acc, _cur) => {
+          const isDist = _cur.Column2 ? false : true;
+          if (isDist) {
+            name = _cur[title].trim();
+            _acc.dist.push({
+              name: name,
+              level: 'dist',
+              affiliation: city,
+              candidates: {
+                cand_2020_01: _cur.Column3,
+                cand_2020_02: _cur.Column4,
+                cand_2020_03: _cur.Column5,
+              },
+              votes: {
+                valid_votes: _cur.Column6,
+                invalid_votes: _cur.Column7,
+                total_votes: _cur.Column8,
+              },
+              voter_turnout: _cur.Column13,
+            });
+          } else {
+            _acc.village.push({
+              name: _cur.Column2,
+              level: 'village',
+              affiliation: name,
+              candidates: {
+                cand_2020_01: _cur.Column3,
+                cand_2020_02: _cur.Column4,
+                cand_2020_03: _cur.Column5,
+              },
+              votes: {
+                valid_votes: _cur.Column6,
+                invalid_votes: _cur.Column7,
+                total_votes: _cur.Column8,
+              },
+              voter_turnout: _cur.Column13,
+            });
+          }
+          return _acc;
+        },
+        { city: [], dist: [], village: [] },
+      );
+    };
+
+    console.log(test('連江縣', data));
+
+    // const levelArr = Object.values(levels);
+    // const levelIdx = levelArr.indexOf(placeLevel);
+    // if (levelIdx === levelArr.length - 1) return;
+
+    // const queryString = querystring.stringify(
+    //   {
+    //     ...querystring.parse(searchParams.toString()),
+    //     [placeLevel]: placeName,
+    //   },
+    //   { skipEmptyString: true },
+    // );
+    // router.push(`${ROUTER.ELECTION_DATA}?${queryString}`);
   };
   return (
     <tr
