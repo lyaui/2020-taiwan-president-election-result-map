da470d7ab04f5e014136bd5697e893ef1d3732ad
Iris Huang <lyaui@outloook.com>
2023-11-27 00:04:53 +0800

    fix(PercentageBar): popover z-index

--- src/components/UI/PercentageBar.tsx
+++ src/components/UI/PercentageBar.tsx
@@ -21,7 +21,7 @@ interface PercentageBar {
 function StatisticsPanel({ groups }: { groups: Group[] }) {
   const total = groups.reduce((_acc, _cur) => _acc + _cur.value || 0, 0);
   return (
-    <ul className='w-[230px] flex flex-col gap-2.5'>
+    <ul className='w-[230px] flex flex-col gap-2.5 relative'>
       {groups.map((_cand) => {
         const percent = ((_cand.value / total) * 100).toFixed(0) + '%';
         return (
@@ -52,48 +52,50 @@ function PercentageBar({
   const handleHideTooltip = () => setIsShowing(false);
 
   return (
-    <Tooltip
-      isShowing={isShowing}
-      onOpen={handleShowTooltip}
-      onClose={handleHideTooltip}
-      label={<StatisticsPanel groups={groups} />}
-    >
-      <Transition
-        appear={true}
-        show={true}
-        enter='transition-opacity duration-75'
-        enterFrom='opacity-0'
-        enterTo='opacity-100'
+    <div className={`relative ${isShowing && 'z-50'}`}>
+      <Tooltip
+        isShowing={isShowing}
+        onOpen={handleShowTooltip}
+        onClose={handleHideTooltip}
+        label={<StatisticsPanel groups={groups} />}
       >
-        <div
-          className='cursor-default py-2'
-          onMouseEnter={handleShowTooltip}
-          onMouseLeave={handleHideTooltip}
+        <Transition
+          appear={true}
+          show={true}
+          enter='transition-opacity duration-75'
+          enterFrom='opacity-0'
+          enterTo='opacity-100'
         >
           <div
-            className='w-full flex overflow-hidden rounded-[50px] bg-gray-200'
-            style={{ height }}
+            className='cursor-default py-2'
+            onMouseEnter={handleShowTooltip}
+            onMouseLeave={handleHideTooltip}
           >
-            {totalValue > 0 &&
-              groups.map((_item) => {
-                const percent = _item.value / totalValue;
-                return (
-                  <span
-                    key={_item.color}
-                    className={` !text-white ${_item.color} text-center caption hover:brightness-110 c-transition`}
-                    style={{
-                      width: percent * 100 + '%',
-                      lineHeight: height + 'px',
-                    }}
-                  >
-                    {showValue && `${(percent * 100).toFixed(0)}%`}
-                  </span>
-                );
-              })}
+            <div
+              className='w-full flex overflow-hidden rounded-[50px] bg-gray-200'
+              style={{ height }}
+            >
+              {totalValue > 0 &&
+                groups.map((_item) => {
+                  const percent = _item.value / totalValue;
+                  return (
+                    <span
+                      key={_item.color}
+                      className={` !text-white ${_item.color} text-center caption hover:brightness-110 c-transition`}
+                      style={{
+                        width: percent * 100 + '%',
+                        lineHeight: height + 'px',
+                      }}
+                    >
+                      {showValue && `${(percent * 100).toFixed(0)}%`}
+                    </span>
+                  );
+                })}
+            </div>
           </div>
-        </div>
-      </Transition>
-    </Tooltip>
+        </Transition>
+      </Tooltip>
+    </div>
   );
 }
 
