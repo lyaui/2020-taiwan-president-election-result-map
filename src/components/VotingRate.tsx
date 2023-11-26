4dd311cec3212c773a2af3725b8c82f5c08c5eac
Iris Huang <lyaui@outloook.com>
2023-11-27 01:03:13 +0800

    style(VotingRate): rwd spacing

--- src/components/VotingRate.tsx
+++ src/components/VotingRate.tsx
@@ -14,7 +14,7 @@ function VotingRate({ votingResult }: { votingResult: VotingResult }) {
   return (
     <div className='flex items-center gap-8 xs:gap-10 bg-white p-4 xs:px-6 xs:py-[30px] rounded-xl'>
       <DonutChart size={125} label='投票率' value={+voter_turnout / 100} />
-      <div className='grid grid-cols-1 xs:grid-cols-2 md:gap-x-12 gap-y-4'>
+      <div className='grid grid-cols-1 xs:grid-cols-2 md:gap-x-10 gap-y-4'>
         {figures.map((_figure) => (
           <div
             key={_figure.label}
