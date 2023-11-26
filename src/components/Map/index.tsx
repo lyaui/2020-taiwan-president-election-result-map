b95aa6ee682432b3b1e1687ab83c77c53b57e6e4
Iris Huang <lyaui@outloook.com>
2023-11-27 01:29:07 +0800

    feat(Map): lol

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -2,7 +2,22 @@ import React from 'react';
 
 function Map() {
   return (
-    <article className='w-[500px] h-[calc(100vh-65px)] bg-gray-400 overflow-auto shrink-0'></article>
+    <article className='w-[500px] h-[calc(100vh-65px)] p-5 bg-gray-400 overflow-auto shrink-0'>
+      <p className='heading-5 !leading-8 whitespace-pre-line'>
+        {`你給我注意聽好
+        
+        我的料理是炸鳳尾蝦就是蝦子裹碎核果去炸的
+        將核果搗碎後裹在蝦肉上 油炸成為金黃色 不但要考慮核果跟蝦肉的比例
+        高低由溫的調節 也需要高度的技術跟經驗的
+        
+        沒錯 這是毫無瑕疵的完美作品
+        不這比完美還要完美 鮮脆可口的核果外衣 經油炸之後 其香味立即倍增
+        堪稱人間第一美味 搭配特製醬汁來食用 味道更是妙不可言
+        
+        醬汁?
+        跟醬汁巧妙的組合 這是這個炸蝦的精華所在`}
+      </p>
+    </article>
   );
 }
 
