c890a4ac1eedacf2dd2b60651b5cc1cee591da5c
Iris Huang <lyaui@outloook.com>
2023-11-28 00:49:50 +0800

    fix(utils): add type condition

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,9 @@
 export function numberWithCommas(num: number) {
+  if (typeof num !== 'number') return num;
   return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 
 export function transCommaStringToNumber(str: string = '') {
+  if (typeof str !== 'string') return str;
   return parseInt(str.split(',').join(''));
 }
