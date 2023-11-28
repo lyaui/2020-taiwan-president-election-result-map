95184dbae12587a993c4cece8214c3317f3838e8
Iris Huang <lyaui@outloook.com>
2023-11-28 16:28:24 +0800

    fix(ChartWrapper): remove title prop

--- src/components/Chart/ChartWrapper.tsx
+++ src/components/Chart/ChartWrapper.tsx
@@ -1,14 +1,12 @@
 import { type ReactNode } from 'react';
 
 interface ChartWrapper {
-  title: string;
   children: ReactNode;
 }
 
-function ChartWrapper({ title, children }: ChartWrapper) {
+function ChartWrapper({ children }: ChartWrapper) {
   return (
     <div className='border-[1px] border-line px-4 py-6 rounded-xl'>
-      {/* <h4 className='heading-5 mb-1'>{title}</h4> */}
       <div className='h-[270px] 2xl:h-[300px]'>{children}</div>
     </div>
   );
