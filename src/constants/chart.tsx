741f9e94544cfea8abb2c5e819af2d1d36c46da6
Iris Huang <lyaui@outloook.com>
2023-11-28 18:38:19 +0800

    feat(constants): add tooltip options

--- src/constants/chart.tsx
+++ src/constants/chart.tsx
@@ -5,27 +5,41 @@ export const partyColors = {
 } as const;
 
 export const options = {
-  responsive: true,
-  maintainAspectRatio: false,
-  plugins: {
-    legend: {
-      position: 'top' as const,
-      align: 'end' as const,
-      labels: {
-        usePointStyle: true,
-        color: '#334155',
-        padding: 20,
-      },
-    },
-    title: {
-      display: true,
-      text: '',
+  responsive: { responsive: true, maintainAspectRatio: false },
+  legend: {
+    position: 'top' as const,
+    align: 'end' as const,
+    labels: {
+      usePointStyle: true,
       color: '#334155',
-      align: 'start',
-      padding: 0,
-      font: {
-        size: 20,
-      },
+      padding: 20,
     },
+  },
+  title: {
+    display: true,
+    color: '#334155',
+    align: 'start' as const,
+    padding: 0,
+    font: {
+      size: 20,
+    },
+  },
+  tooltip: {
+    mode: 'index',
+    boxWidth: 8,
+    boxHeight: 8,
+    boxPadding: 8,
+    padding: 16,
+    borderWidth: 1,
+    usePointStyle: true,
+    borderColor: '#DEE2E6',
+    titleColor: '#334155',
+    titleMarginBottom: 10,
+    titleFont: { size: 18 },
+    bodyColor: '#334155',
+    bodySpacing: 10,
+    bodyAlign: 'left',
+    bodyFont: { size: 15, weight: 'normal' },
+    backgroundColor: '#FFFFFF',
   },
 };
