75d469a0fd9e7e5cab565ef5ed6e38b19824c6e1
Iris Huang <lyaui@outloook.com>
2023-12-01 01:36:30 +0800

    feat(utils): add className combination function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,10 @@
+import { ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
 export function numberWithCommas(num: number) {
   if (typeof num !== 'number') return num;
   return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
