0ad7cefba8d791fdfdeaeea2206d9fa128259616
Iris Huang <lyaui@outloook.com>
2023-12-03 12:04:59 +0800

    feat(Button): add cva

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -1,44 +1,56 @@
 import { type ComponentPropsWithoutRef } from 'react';
+import { cva, type VariantProps } from 'class-variance-authority';
 
-import { cn } from '@/utils';
+const buttonVariants = cva(
+  'flex-center body-small rounded-[6px] c-transition',
+  {
+    variants: {
+      color: {
+        primary: [
+          'bg-primary',
+          '!text-white',
+          'hover:bg-primary-light',
+          'active:bg-primary',
+        ],
+        secondary: [
+          'bg-background',
+          'text-text-primary',
+          'hover:bg-hover',
+          'active:bg-background',
+        ],
+      },
+      size: {
+        small: ['px-3', 'py-1'],
+        medium: ['px-4', 'py-2'],
+        large: ['px-6', 'py-4'],
+      },
+    },
+    compoundVariants: [{ color: 'primary', size: 'medium' }],
+    defaultVariants: {
+      color: 'primary',
+      size: 'medium',
+    },
+  }
+);
 
-interface ButtonProps extends ComponentPropsWithoutRef<'button'> {
+interface ButtonProps
+  extends ComponentPropsWithoutRef<'button'>,
+    VariantProps<typeof buttonVariants> {
   children: string;
   size?: 'small' | 'medium' | 'large';
   color?: 'primary' | 'secondary';
   className?: string;
 }
 
-function Button(props: ButtonProps) {
-  const {
-    children,
-    size = 'medium',
-    color = 'primary',
-    className = '',
-    ...others
-  } = props;
-
-  const sizeClasses = {
-    small: 'px-3 py-1',
-    medium: 'px-4 py-2',
-    large: 'px-6 py-4',
-  }[size];
-
-  const colorClasses = {
-    primary: 'bg-primary text-white hover:bg-primary-light active:bg-primary',
-    secondary:
-      'bg-background text-text-primary hover:bg-hover active:bg-background',
-  }[color];
-
-  const classes = cn(
-    'flex-center body-small rounded-[6px] c-transition',
-    sizeClasses,
-    colorClasses,
-    className
-  );
-
+function Button({
+  children,
+  size = 'medium',
+  color = 'primary',
+  className = '',
+  ...others
+}: ButtonProps) {
   return (
-    <button className={classes} {...others}>
+    <button className={buttonVariants({ color, size, className })} {...others}>
       {children}
     </button>
   );
