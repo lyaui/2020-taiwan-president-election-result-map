078d3e5c473f520989afb1830ac7c4783b756ea2
Iris Huang <lyaui@outloook.com>
2023-12-04 21:40:35 +0800

    fix: remove oops img

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,17 +1,8 @@
 import Image from 'next/image';
-import oopsImg from 'public/assets/images/oops.png';
 
 function Map() {
   return (
-    <article className='h-[calc(100vh-65px)] w-full shrink-0 overflow-auto bg-gray-400 p-5 2xl:w-[500px]'>
-      <Image
-        src={oopsImg}
-        width={350}
-        height={350}
-        alt='oooops'
-        draggable={false}
-      />
-    </article>
+    <article className="h-[calc(100vh-65px)] w-full shrink-0 overflow-auto bg-gray-400 p-5 2xl:w-[500px]"></article>
   );
 }
 
